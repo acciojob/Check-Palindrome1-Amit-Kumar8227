bcc46c1032795ee5f3d169eec3c4f78caa530938
Amit Kumar <127511786+Amit-Kumar8227@users.noreply.github.com>
2023-05-18 20:48:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	var newstr=str.toUpperCase();
+	var revstr=str.reverse();
+	if(newstr===revstr){
+		return true;
+	}
+	else {
+		return false;
+	}
 
 }
 module.exports = palindrome
